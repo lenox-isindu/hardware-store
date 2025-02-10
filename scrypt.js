6b3c4050ea3cf36f980a71f80bd20388143ffe69
lenox-isindu <lenox1739@gmail.com>
2025-02-11 00:38:38 +0300

    initial commit

---
+++ scrypt.js
@@ -0,0 +1,10 @@
+let cartCount = 0;
+
+function addToCart() {
+    cartCount++;
+    document.querySelector('.cart-badge').textContent = cartCount;
+}
+
+function openCart() {
+    alert(`You have ${cartCount} item(s) in your cart.`);
+}
